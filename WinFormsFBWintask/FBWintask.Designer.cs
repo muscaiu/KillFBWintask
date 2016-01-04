8c6b162ed9469ef8d1ba112758cc565116a8393d
Cristian Muscalu <muscaiu@yahoo.com>
2016-01-04 15:13:05 +0200

    Working Version 1.0

---
+++ WinFormsFBWintask/FBWintask.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace WinFormsFBWintask
+{
+    partial class FBWintask
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FBWintask));
+            this.BtnConnect = new System.Windows.Forms.Button();
+            this.BtnStart = new System.Windows.Forms.Button();
+            this.LblConnection = new System.Windows.Forms.Label();
+            this.LblTask = new System.Windows.Forms.Label();
+            this.LblStartingRecords = new System.Windows.Forms.Label();
+            this.LblRemainingRecords = new System.Windows.Forms.Label();
+            this.LblTotalTime = new System.Windows.Forms.Label();
+            this.TextBoxFb_Table = new System.Windows.Forms.TextBox();
+            this.LblResolvedRecords = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.ListBoxArchive = new System.Windows.Forms.ListBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // BtnConnect
+            // 
+            this.BtnConnect.Location = new System.Drawing.Point(13, 13);
+            this.BtnConnect.Name = "BtnConnect";
+            this.BtnConnect.Size = new System.Drawing.Size(106, 50);
+            this.BtnConnect.TabIndex = 0;
+            this.BtnConnect.Text = "Connect";
+            this.BtnConnect.UseVisualStyleBackColor = true;
+            this.BtnConnect.Click += new System.EventHandler(this.BtnConnect_Click);
+            // 
+            // BtnStart
+            // 
+            this.BtnStart.Enabled = false;
+            this.BtnStart.Location = new System.Drawing.Point(12, 69);
+            this.BtnStart.Name = "BtnStart";
+            this.BtnStart.Size = new System.Drawing.Size(107, 46);
+            this.BtnStart.TabIndex = 1;
+            this.BtnStart.Text = "Start Task";
+            this.BtnStart.UseVisualStyleBackColor = true;
+            this.BtnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // LblConnection
+            // 
+            this.LblConnection.AutoSize = true;
+            this.LblConnection.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblConnection.ForeColor = System.Drawing.SystemColors.ActiveBorder;
+            this.LblConnection.Location = new System.Drawing.Point(125, 27);
+            this.LblConnection.Name = "LblConnection";
+            this.LblConnection.Size = new System.Drawing.Size(119, 20);
+            this.LblConnection.TabIndex = 2;
+            this.LblConnection.Text = "Disconnected";
+            // 
+            // LblTask
+            // 
+            this.LblTask.AutoSize = true;
+            this.LblTask.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblTask.ForeColor = System.Drawing.SystemColors.ActiveBorder;
+            this.LblTask.Location = new System.Drawing.Point(125, 81);
+            this.LblTask.Name = "LblTask";
+            this.LblTask.Size = new System.Drawing.Size(77, 20);
+            this.LblTask.TabIndex = 3;
+            this.LblTask.Text = "Task Off";
+            // 
+            // LblStartingRecords
+            // 
+            this.LblStartingRecords.AutoSize = true;
+            this.LblStartingRecords.ForeColor = System.Drawing.Color.White;
+            this.LblStartingRecords.Location = new System.Drawing.Point(10, 171);
+            this.LblStartingRecords.Name = "LblStartingRecords";
+            this.LblStartingRecords.Size = new System.Drawing.Size(92, 13);
+            this.LblStartingRecords.TabIndex = 5;
+            this.LblStartingRecords.Text = "Starting Records: ";
+            // 
+            // LblRemainingRecords
+            // 
+            this.LblRemainingRecords.AutoSize = true;
+            this.LblRemainingRecords.ForeColor = System.Drawing.Color.White;
+            this.LblRemainingRecords.Location = new System.Drawing.Point(9, 197);
+            this.LblRemainingRecords.Name = "LblRemainingRecords";
+            this.LblRemainingRecords.Size = new System.Drawing.Size(106, 13);
+            this.LblRemainingRecords.TabIndex = 6;
+            this.LblRemainingRecords.Text = "Remaining Records: ";
+            // 
+            // LblTotalTime
+            // 
+            this.LblTotalTime.AutoSize = true;
+            this.LblTotalTime.ForeColor = System.Drawing.Color.White;
+            this.LblTotalTime.Location = new System.Drawing.Point(9, 254);
+            this.LblTotalTime.Name = "LblTotalTime";
+            this.LblTotalTime.Size = new System.Drawing.Size(63, 13);
+            this.LblTotalTime.TabIndex = 7;
+            this.LblTotalTime.Text = "Total Time: ";
+            // 
+            // TextBoxFb_Table
+            // 
+            this.TextBoxFb_Table.Location = new System.Drawing.Point(13, 131);
+            this.TextBoxFb_Table.Name = "TextBoxFb_Table";
+            this.TextBoxFb_Table.Size = new System.Drawing.Size(239, 20);
+            this.TextBoxFb_Table.TabIndex = 8;
+            // 
+            // LblResolvedRecords
+            // 
+            this.LblResolvedRecords.AutoSize = true;
+            this.LblResolvedRecords.ForeColor = System.Drawing.Color.White;
+            this.LblResolvedRecords.Location = new System.Drawing.Point(9, 227);
+            this.LblResolvedRecords.Name = "LblResolvedRecords";
+            this.LblResolvedRecords.Size = new System.Drawing.Size(101, 13);
+            this.LblResolvedRecords.TabIndex = 9;
+            this.LblResolvedRecords.Text = "Resolved Records: ";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // ListBoxArchive
+            // 
+            this.ListBoxArchive.BackColor = System.Drawing.Color.LightGray;
+            this.ListBoxArchive.ForeColor = System.Drawing.Color.Black;
+            this.ListBoxArchive.FormattingEnabled = true;
+            this.ListBoxArchive.Location = new System.Drawing.Point(279, 12);
+            this.ListBoxArchive.Name = "ListBoxArchive";
+            this.ListBoxArchive.Size = new System.Drawing.Size(239, 277);
+            this.ListBoxArchive.TabIndex = 10;
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 10000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 120000;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // FBWintask
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(93)))), ((int)(((byte)(154)))));
+            this.ClientSize = new System.Drawing.Size(530, 299);
+            this.Controls.Add(this.ListBoxArchive);
+            this.Controls.Add(this.LblResolvedRecords);
+            this.Controls.Add(this.TextBoxFb_Table);
+            this.Controls.Add(this.LblTotalTime);
+            this.Controls.Add(this.LblRemainingRecords);
+            this.Controls.Add(this.LblStartingRecords);
+            this.Controls.Add(this.LblTask);
+            this.Controls.Add(this.LblConnection);
+            this.Controls.Add(this.BtnStart);
+            this.Controls.Add(this.BtnConnect);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FBWintask";
+            this.Text = "FBWintask";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnConnect;
+        private System.Windows.Forms.Button BtnStart;
+        private System.Windows.Forms.Label LblConnection;
+        private System.Windows.Forms.Label LblTask;
+        private System.Windows.Forms.Label LblStartingRecords;
+        private System.Windows.Forms.Label LblRemainingRecords;
+        private System.Windows.Forms.Label LblTotalTime;
+        private System.Windows.Forms.TextBox TextBoxFb_Table;
+        private System.Windows.Forms.Label LblResolvedRecords;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ListBox ListBoxArchive;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ WinFormsFBWintask/FBWintask.cs
@@ -0,0 +1,349 @@
+﻿using System;
+using System.Data.Odbc;
+using System.Diagnostics;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WinFormsFBWintask
+{
+    public partial class FBWintask : Form
+    {
+        public FBWintask()
+        {
+            InitializeComponent();
+        }
+
+        private OdbcConnection _db = new OdbcConnection(_connectionString);
+
+        private static string _connectionString =
+            "DRIVER={MySQL ODBC 3.51 Driver};SERVER=x.x.x.x;UID=x;PWD=x;DATABASE=x;";
+
+        private bool _connected;
+        private bool _startStatus;
+        private bool _checkStartingRecords = true; //to get the starting records only once at connect 
+
+        private int _startingCount;
+        private int _remainingCount;
+        private int _resolvedCount;
+        private int _oldResolvedCount;
+        private int _newResolvedCount;
+        private int _check_ban;
+        private int _check_email;
+
+        private int _totalTimeSeconds;
+        private int _totalTimeMinutes;
+        private int _totalTimeHours;
+
+        Process proc = new Process();
+
+        //Timer1 Tick
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            LblTotalTime.Text = "Total Time: " + _totalTimeHours + " : " + _totalTimeMinutes + " : " + _totalTimeSeconds;
+
+            //reset seconds
+            if (_totalTimeSeconds >= 59)
+            {
+                _totalTimeSeconds = -1;
+                _totalTimeMinutes = _totalTimeMinutes + 1;
+            }
+            //reset minutes
+            if (_totalTimeMinutes >= 59)
+            {
+                _totalTimeMinutes = 0;
+                _totalTimeHours = _totalTimeHours + 1;
+            }
+
+            _totalTimeSeconds = _totalTimeSeconds + 1;
+        }
+        //Timer2 Tick
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            //check all stuff every 10 seconds
+            CheckRemainingRecords();
+            CheckProcess();
+
+            //Get _newResolvedCount 
+            _newResolvedCount = _resolvedCount;
+        }
+        //Timer3 Tick every 2 minutes kill Task and restart it
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            CheckBan(); //check database for new values
+
+            if (_oldResolvedCount == _newResolvedCount)
+            {
+                //kill Task
+                try
+                {
+                    foreach (Process proc in Process.GetProcessesByName("TaskExec"))
+                    {
+                        proc.Kill();
+                        _startStatus = false;
+                    }
+                    foreach (Process proc in Process.GetProcessesByName("firefox"))
+                    {
+                        proc.Kill();
+                        _startStatus = false;
+                    }
+                    if (_startStatus == false)
+                    {
+                        timer1.Stop();
+                        timer2.Stop();
+                        timer3.Stop();
+                        ListBoxArchive.Items.Add("Stopped: " + DateTime.Now);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Could not kill! " + ex.ToString());
+                }
+
+                ////////////Restart Task
+                Thread.Sleep(5000);
+                //start timers
+                timer1.Start();
+                timer2.Start();
+                timer3.Start();
+                try
+                {
+                    //start Rob
+                    proc.StartInfo.FileName = @"D:\GoogleDrive\Wintask\Facebook\facebook_profiles.rob";
+                    proc.StartInfo.UseShellExecute = true;
+                    proc.Start();
+                    _startStatus = true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Could not start Rob! " + ex.ToString());
+                }
+            }
+
+            _oldResolvedCount = _resolvedCount;
+        }
+
+        //BtnConnect
+        private void BtnConnect_Click(object sender, EventArgs e)
+        {
+            //connect
+            if (_connected == false)
+            {
+                try
+                {
+                    _db.Open();
+                    _connected = true;
+
+                    LblConnection.Text = "Connected";
+                    LblConnection.ForeColor = Color.White;
+                    BtnConnect.Text = "Disconnect";
+
+                    BtnStart.Enabled = true;
+                    BtnConnect.Enabled = false;
+                    ListBoxArchive.Items.Add("Connected at: " + DateTime.Now);
+
+                    //get the starting records only once
+                    if (_checkStartingRecords == true)
+                    {
+                        CheckStartingRecords();
+                        _checkStartingRecords = false;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Failed to Connenct!" + ex.ToString());
+                }
+            }
+            //disconnect
+            else if (_connected == true)
+            {
+                _db.Close();
+                _connected = false;
+                LblConnection.Text = "Disconnected";
+                LblConnection.ForeColor = Color.Gray;
+                BtnConnect.Text = "Connect";
+
+                //disable Start Button
+                BtnStart.Enabled = false;
+
+                //stop timers
+                timer1.Stop();
+                timer2.Stop();
+                timer3.Stop();
+                ListBoxArchive.Items.Add("Disconnected: " + DateTime.Now);
+
+            }
+        }
+
+        //BtnSart
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            if (TextBoxFb_Table.Text == "")
+            {
+                MessageBox.Show("Enter database table name!", "Can't Start!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else
+            {
+                if (_startStatus == false)
+                {
+                    BtnStart.Text = "Kill Task";
+                    //start timers
+                    timer1.Start();
+                    timer2.Start();
+                    timer3.Start();
+                    try
+                    {
+                        //start Rob
+                        proc.StartInfo.FileName = @"D:\GoogleDrive\Wintask\Facebook\facebook_profiles.rob";
+                        proc.StartInfo.UseShellExecute = true;
+                        proc.Start();
+                        _startStatus = true;
+                        ListBoxArchive.Items.Add("Task Started at: " + DateTime.Now);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Could not start Rob! " + ex.ToString());
+                    }
+                }
+
+                else if (_startStatus == true)
+                {
+                    BtnStart.Text = "Start Task";
+                    try
+                    {
+                        foreach (Process proc in Process.GetProcessesByName("TaskExec"))
+                        {
+                            proc.Kill();
+                            _startStatus = false;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Could not kill! " + ex.ToString());
+                    }
+                }
+            }
+
+        }
+
+
+        //Check Process
+        public void CheckProcess()
+        {
+            Process[] pname = Process.GetProcessesByName("TaskExec");
+            if (pname.Length == 0)
+            {
+                BtnStart.Text = "Start Task";
+                LblTask.Text = "Rob Off";
+                LblTask.ForeColor = Color.Gray;
+                _startStatus = false;
+            }
+            else
+            {
+                BtnStart.Text = "Kill Task";
+                LblTask.Text = "Rob On";
+                LblTask.ForeColor = Color.White;
+                _startStatus = true;
+            }
+        }
+
+        //Check Starting records when i click Start
+        public void CheckStartingRecords()
+        {
+            string fbTable = TextBoxFb_Table.Text;
+            string strQuery = "SELECT COUNT(*) AS TimeCheck FROM " + fbTable + " WHERE Finished = 0";
+            try
+            {
+                OdbcCommand cmd = new OdbcCommand(strQuery, _db);
+                _startingCount = Convert.ToInt32(cmd.ExecuteScalar());
+                LblStartingRecords.Text = "Starting Records: " + _startingCount;
+            }
+            catch (Exception)
+            {
+                //Btn_StartTimers.Content = "Start Timers";
+            }
+        }
+        //Check Remaining Records every secods
+        public void CheckRemainingRecords()
+        {
+            string fbTable = TextBoxFb_Table.Text;
+            string strQuery = "SELECT COUNT(*) AS TimeCheck FROM " + fbTable + " WHERE Finished = 0";
+
+            try
+            {
+                //execute query for remaining count
+                OdbcCommand cmd = new OdbcCommand(strQuery, _db);
+                _remainingCount = Convert.ToInt32(cmd.ExecuteScalar());
+                LblRemainingRecords.Text = "Remaining Records: " + _remainingCount;
+            }
+            catch (Exception)
+            {
+                //Btn_StartTimers.Content = "Start Timers";
+            }
+
+            //End of Program
+            if (_remainingCount == 0)
+            {
+                timer1.Stop();
+                timer2.Stop();
+                timer3.Stop();
+                ListBoxArchive.Items.Add("All Records Finished!");
+                ListBoxArchive.Items.Add("Finished at: " + DateTime.Now);
+                MessageBox.Show("All Records Finished!");
+                _startStatus = false;
+            }
+            //Check Resolved Records
+            _resolvedCount = _startingCount - _remainingCount;
+            LblResolvedRecords.Text = "Resolved Records: " + _resolvedCount;
+        }
+
+        //Check Database every 2 minutes
+        public void CheckBan()
+        {
+            string fbTable = TextBoxFb_Table.Text;
+            string check_if_ban = "SELECT COUNT(*) AS TimeCheck FROM " + fbTable + " WHERE Finished = 2";
+            string check_email = "SELECT COUNT(*) AS TimeCheck FROM " + fbTable + " WHERE Finished = 3";
+
+            //execute query to see if any field has the value 2
+            OdbcCommand check_ban_query = new OdbcCommand(check_if_ban, _db);
+            _check_ban = Convert.ToInt32(check_ban_query.ExecuteScalar());
+            //execute query to see if any field has the value 3
+            OdbcCommand check_email_query = new OdbcCommand(check_email, _db);
+            _check_email = Convert.ToInt32(check_email_query.ExecuteScalar());
+
+            //Verify in Databse if Account is banned
+            if (_check_ban >= 1)
+            {
+                timer1.Stop(); //pause timer
+                timer2.Stop();
+                ListBoxArchive.Items.Add("Stopped at: " + DateTime.Now);
+                ListBoxArchive.Items.Add("Change the Facebook Account!");
+                BtnStart.Text = "Restart Task";
+                _startStatus = false;
+            }
+            else
+            {
+                timer1.Start();
+                timer2.Start();
+                _startStatus = true;
+            }
+
+            //Verify in Databse if email is not verified
+            if (_check_email >= 1)
+            {
+                timer1.Stop(); //pause timer
+                timer2.Stop();
+                ListBoxArchive.Items.Add("Stopped at: " + DateTime.Now);
+                ListBoxArchive.Items.Add("Email check detected!");
+                BtnStart.Text = "Restart Task";
+                _startStatus = false;
+            }
+            else
+            {
+                timer1.Start();
+                timer2.Start();
+                _startStatus = true;
+            }
+        }
+    }
+}
